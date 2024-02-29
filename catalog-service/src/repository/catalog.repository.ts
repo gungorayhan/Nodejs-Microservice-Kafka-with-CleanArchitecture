4fe3f7c470c00c2c67b44c09d42f7a96940192c0
AYHAN GÜNGÖR <gngr.ayhan@gmail.com>
2024-02-29 15:12:20 +0300

    prisma(postgresql) ve docker-compose entegrasyonları yapıldı. sistem postman ile test edildi

--- catalog-service/src/repository/catalog.repository.ts
+++ catalog-service/src/repository/catalog.repository.ts
@@ -1,22 +1,56 @@
+import { PrismaClient } from "@prisma/client";
 import { ICatalogRepository } from "../interface/catalogRepository.interface";
 import { Product } from "../models/product.model";
+import { ProductFactory } from "../utils/fixtures";
+import { off } from "process";
 
 
-export class CatalogRepository implements ICatalogRepository{
-    create(data: Product): Promise<Product> {
-        throw new Error("Method not implemented.");
+export class CatalogRepository implements ICatalogRepository {
+
+    _prisma: PrismaClient
+    constructor() {
+        this._prisma = new PrismaClient()
     }
-    update(data: Product): Promise<Product> {
-        throw new Error("Method not implemented.");
+
+    async create(data: Product): Promise<Product> {
+        //    const product = ProductFactory.build()
+        //    return Promise.resolve(product)
+        return this._prisma.product.create({ data })
     }
-    delete(id: any) {
-        throw new Error("Method not implemented.");
+    async update(data: Product): Promise<Product> {
+        // const product = ProductFactory.build()
+        // return Promise.resolve(product)
+        return this._prisma.product.update({
+            where: { id: data.id },
+            data,
+        })
     }
-    find(): Promise<Product[]> {
-        throw new Error("Method not implemented.");
+    async delete(id: any) {
+        //     const product = ProductFactory.build()
+        //    return Promise.resolve(product)
+        return this._prisma.product.delete({
+            where: { id }
+        })
     }
-    findOne(id: number): Promise<Product> {
-        throw new Error("Method not implemented.");
+    async find(limit: number, offset: number): Promise<Product[]> {
+        //     const products = ProductFactory.buildList(limit)
+        //    return Promise.resolve(products)
+        return this._prisma.product.findMany({
+            take:limit,
+            skip:offset
+        })
+    }
+    async findOne(id: number): Promise<Product> {
+        // const product = ProductFactory.build()
+        // return Promise.resolve(product)
+        const product= await this._prisma.product.findFirst({
+            where:{id}
+        })
+
+        if(product){
+            return Promise.resolve(product)
+        }
+        throw new Error("product not found")
     }
 
 }
